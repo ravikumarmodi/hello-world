5c8cd3c9e60c26a4fb2f8670cef316b384d99009
akshyaganesh <42618400+akshyaganesh@users.noreply.github.com>
2024-03-13 16:15:11 +0530

    Update main.tf
    corrected the code

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,16 +1,26 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
-  }
-
-  required_version = ">=0.14.9"
-
-}
-
 provider "aws" {
-  version = "~>3.0"
-  region  = "east-us-1"
+  region = "us-east-1"
+}
+ 
+# Create an S3 bucket
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "kyn_consult_adai_java_app_demo"
+  acl    = "public-read"
+ 
+  website {
+    index_document = "index.html"
+  }
+}
+ 
+# Upload website files to the bucket
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  key    = "index.html"
+  source = "path/to/your/index.html"
+  acl    = "public-read"
+}
+ 
+# Output the website endpoint
+output "website_url" {
+  value = aws_s3_bucket.demo_bucket.website_endpoint
 }
