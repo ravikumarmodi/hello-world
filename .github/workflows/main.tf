8bd8f53d1479d34d8c13274f4f5338ffc1ed5006
ravikumarmodi <35724012+ravikumarmodi@users.noreply.github.com>
2024-03-14 11:45:46 +0530

    Update main.tf
    update

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -26,11 +26,6 @@ resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.demo-bucket.id
   key    = "index.jsp"
   source = "webapp/src/main/webapp"
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("webapp/src/main/webapp")
 }
  
 # Output the website endpoint
